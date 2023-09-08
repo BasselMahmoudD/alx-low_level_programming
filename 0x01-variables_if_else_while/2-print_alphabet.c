a77dcb0ab420834da9ad7b41fb6a272dd2b72af9
BasselMahmoudD <Bassel@yahoo.com>
2023-09-08 16:58:40 -0700

    ok

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print character from a to z
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char chr = 'a';
+
+	while (chr <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
