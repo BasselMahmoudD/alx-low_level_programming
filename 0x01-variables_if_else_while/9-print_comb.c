e653c20811c22ec5a051ae0d3aa000c9ac14815f
BasselMahmoudD <Bassel@yahoo.com>
2023-09-08 18:14:08 -0700

    ok

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print from 1 to 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit < 58)
+	{
+		putchar(digit);
+		putchar(',');
+		putchar(' ');
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
