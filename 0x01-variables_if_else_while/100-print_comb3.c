9cd2a752816e06f8ee6e986ca40e1dbde38f2c70
BasselMahmoudD <Bassel@yahoo.com>
2023-09-08 18:29:54 -0700

    ok

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print combination of tow numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit1 = 48;
+
+	while (digit1 < 58)
+	{
+		putchar(digit1);
+		int digit2 = digit1 + 1;
+
+		while (digit2 < 58)
+		{
+			putchar(digit2);
+			putchar(',');
+			putchar(' ');
+			digit2++;
+		}
+		digit1++;
+	}
+	putchar('\n');
+	return (0);
+}
