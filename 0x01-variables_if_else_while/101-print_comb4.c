858ae68d7168c5eb022426a2195e8a528b823516
BasselMahmoudD <Bassel@yahoo.com>
2023-09-08 19:08:34 -0700

    ok

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print combination of three numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit1 = 48;
+	int digit2, digit3;
+
+	while (digit1 < 58)
+	{
+		digit2 = digit1 + 1;
+		while (digit2 < 58)
+		{
+			digit3 = digit2 + 1;
+			while (digit3 < 58)
+			{
+				putchar(digit1);
+				putchar(digit2);
+				putchar(digit3);
+				if (digit1 != 55)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		putchar('\n');
+	}
+	return (0);
+}
