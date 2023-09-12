e69be5f791c36ed71d8af59004a10958dd433ee7
BasselMahmoudD <Bassel@yahoo.com>
2023-09-12 15:51:54 -0700

    ok

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+
+/**
+ *add - add tow numbers
+ *
+ *@a: frist input
+ *@b: second input
+ *
+ * Return: sum of a and b
+ *
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
