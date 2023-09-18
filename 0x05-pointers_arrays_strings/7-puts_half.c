78932f1c558ee5533d1e004b96bc10cf9878c149
BasselMahmoudD <Bassel@yahoo.com>
2023-09-18 13:36:32 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts_half - print hafl of arr
+ *
+ * @str: input
+*/
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	for (i /= 2; str[i] ! = '\0'; i++)
+	       _putchar(str[i]);
+	_putchar('\n');	
+}
