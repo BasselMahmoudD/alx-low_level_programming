ea7f51e24e83f0c94555e934ee43d7339b968340
BasselMahmoudD <Bassel@yahoo.com>
2023-09-21 12:44:00 -0700

    ok

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *rot13 - encoded a string using rot13
+ * @s: input
+ *
+ * Return: Encoded string
+*/
+char *rot13(char *s)
+{
+	int i;
+
+	char rot13[] = "abcdefghijklmnopqrstuvWxyzABCDEFGHIJKLMNOPQRTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRTSUVWXYZABCDEFGHIJKLM";
+	char *ptr = s;
+
+	while (*s)
+	{
+		for (i = 0; i <= 52; i++)
+		{
+			if (*s == rot13[i])
+			{
+				*s = ROT13[i];
+				break;
+			}
+		}
+		s++;
+	}
+	return (ptr);
+}
