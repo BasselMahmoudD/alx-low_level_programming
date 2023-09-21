8e2ce524ba8b5a329955d13eb6e0fe903dd956c5
BasselMahmoudD <Bassel@yahoo.com>
2023-09-21 12:50:25 -0700

    ok

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,7 +10,7 @@ char *rot13(char *s)
 {
 	int i;
 
-	char rot13[] = "abcdefghijklmnopqrstuvWxyzABCDEFGHIJKLMNOPQRTUVWXYZ";
+	char rot13[] = "abcdefghijklmnopqrstuvWxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 	char *ptr = s;
 
