3409a7921811623573afd4340c4e752356505996
BasselMahmoudD <Bassel@yahoo.com>
2023-09-21 11:11:42 -0700

    ok

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copy tow string
+ *
+ * @dest: input1
+ * @src: input2
+ * @n: input3
+ *
+ * Return: dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && srce[i] != '\0'; i++)
+		dest[i] = src[i];
+	return (dest);
+}
