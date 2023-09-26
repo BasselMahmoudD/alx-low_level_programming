7eaab209f50db4ff454b573216f7ff68a55a3a74
BasselMahmoudD <Bassel@yahoo.com>
2023-09-26 15:41:15 -0700

    ok

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strstr - Write a function that locates a substring.
+ *
+ * @haystack: input1
+ * @needle: input2
+ *
+ * Return: char
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	
+}
